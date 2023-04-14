2ec8cd501eacfcdf504998fe455d03a6dcf551c6
David Ortega <dfortega@gasco.cl>
2023-04-14 00:05:09 -0400

    Se añade logica de colores apra los usuarios que se registren en el chat, se añaden atributos a la clase Mensaje para persistir en algunas propiedades

--- src/main/java/com/backend/chat/controllers/ChatController.java
+++ src/main/java/com/backend/chat/controllers/ChatController.java
@@ -7,19 +7,27 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.util.Date;
+import java.util.Random;
 
 @Controller
 public class ChatController {
 
 
+    private String[] colores = {"red", "green", "blue", "magneta", "purple", "orange"};
+
     @MessageMapping("/mensaje")
     @SendTo("/chat/mensaje")
     public Mensaje recibirMensaje(Mensaje mensaje){
         mensaje.setFecha(new Date().getTime());
-        mensaje.setTexto("Recibido por el Broker: "+ mensaje.getTexto());
+        if (mensaje.getTipo().equals("NUEVO_USUARIO")){
+            mensaje.setColor(colores[new Random().nextInt(colores.length)]);
+            mensaje.setTexto("nuevo usuario");
+        }
 
         return mensaje;
     }
+
+
 
 
 
