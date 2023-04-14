642cc0ba8db3f83473c9714666eb7af543d4b466
David Ortega <dfortega@gasco.cl>
2023-04-13 22:04:37 -0400

    Subiendo proyecto vacio

---
+++ src/main/java/com/backend/chat/BackendChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.backend.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendChatAppApplication.class, args);
+    }
+
+}
